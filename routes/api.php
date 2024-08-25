2f4c4f656903e837a27bd2e1efd2fbd5c3186edf
AXUSLK <thilina.lyceum@gmail.com>
2024-08-25 12:02:43 +0530

    🛂 Implement authentication and authorization using Sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
-Route::apiResource('products', ProductController::class);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('products', ProductController::class);
+});
